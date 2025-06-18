e1a053322bf9d740a4297d61920d43f477e6621d
Vicente Rojas <vrojasaranda@gmail.com>
2025-06-18 11:45:22 -0400

    feat: add buttons to change background weather
    - Add buttons to change the background weather
    - Add hightlight on background of each icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,33 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'relaxTime';
   weatherType = 'sunset';
+  @ViewChild('sunsetIcon') sunsetIconRef!: ElementRef;
+  @ViewChild('cloudyIcon') cloudyIconRef!: ElementRef;
+  @ViewChild('rainyIcon') rainyIconRef!: ElementRef;
+  @ViewChild('shinyIcon') shinyIconRef!: ElementRef;
+
+  highlightTop = 0;
+  highlightLeft = 0;
+  highlightWidth = 0;
+  highlightHeight = 0;
+  setWeatherType(type: string) {
+    this.weatherType = type;
+    setTimeout(() => this.updateWeatherHighlight());
+  }
+  updateWeatherHighlight() {
+    let iconRef: ElementRef;
+    switch (this.weatherType) {
+      case 'sunset': iconRef = this.sunsetIconRef; break;
+      case 'cloudy': iconRef = this.cloudyIconRef; break;
+      case 'rainy': iconRef = this.rainyIconRef; break;
+      case 'shiny': iconRef = this.shinyIconRef; break;
+      default: return;
+    }
+    const el = iconRef.nativeElement;
+    this.highlightTop = el.offsetTop;
+    this.highlightLeft = el.offsetLeft;
+    this.highlightWidth = el.offsetWidth;
+    this.highlightHeight = el.offsetHeight;
+    this.cdr.detectChanges();
+  }
 }
