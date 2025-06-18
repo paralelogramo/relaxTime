6f3d8e55e267169e099af5252f1e8583a20b9188
Vicente Rojas <vrojasaranda@gmail.com>
2025-06-18 11:17:56 -0400

    feat: add location service for weather and geolocation support
    - Provides a method to get latitude and longitude to use in other services

---
+++ src/app/services/location.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocationService {
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  getCurrentPosition(): Promise<{ lat: number; lon: number }> {
+    return new Promise((resolve, reject) => {
+      if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            resolve({
+              lat: position.coords.latitude,
+              lon: position.coords.longitude,
+            });
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      } else {
+        reject('Geolocalización no soportada en tu navegador.');
+      }
+    });
+  }
+
+}
